3d1a80ba2458ed4d78748839ff2ec2a30b6f258f
Nick Baynham <nickbaynham@gmail.com>
2023-08-29 14:26:31 -0400

    Added JUnit 5 Jupiter support and tests

--- src/test/java/org/testautomation/TestMain.java
+++ src/test/java/org/testautomation/TestMain.java
@@ -1,13 +1,41 @@
 package org.testautomation;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.slf4j.Logger;
+
+import static java.lang.invoke.MethodHandles.lookup;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.slf4j.LoggerFactory.getLogger;
 
 public class TestMain {
-    public static void main(String[] args) {
-        System.out.println("Launching Chrome");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.google.com");
+    static final Logger log = getLogger(lookup().lookupClass());
+    private WebDriver driver;
+
+    @BeforeEach
+    void setup() {
+        log.info("Launching the Chrome Browser.");
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void tearDown() {
+        log.info("Terminating the Chrome Browser.");
         driver.quit();
+    }
+
+    @Test
+    void test() {
+        log.info("Executing Demo Test");
+        String URL = "https://bonigarcia.dev/selenium-webdriver-java/";
+        driver.get(URL);
+        String title = driver.getTitle();
+        log.info("The title of {} is {}", URL, title);
+
+        // Verifying
+        assertThat(title).isEqualTo("Hands-On Selenium WebDriver with Java");
     }
 }
